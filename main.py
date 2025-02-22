c15900283a12517f2d20b32d79c17ed894d49b82
zlaubacher <zlaubacher@gmail.com>
2025-02-22 17:42:54 -0500

    D: removed the word count function from main.py and sent it to a bespoke stats.py file, replacing it with import in main.py.

--- main.py
+++ main.py
@@ -5,12 +5,7 @@ def get_book_text(location):
         contents = book.read()
         return contents
 
-def word_count(contents):
-    #sets up a list called total_words with each individual word from the book passed into the function
-    total_words = contents.split()
-    #counts up the length of the list in order to establish a word count
-    num_words = len(total_words)
-    return num_words
+from stats import word_count
 
 def main():
     #defines the file path for the book to be read and creates it as a variable
